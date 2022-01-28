1b59096f937dff06ce92365f8402b34a6bf3eac5
Alex Schmelkin <alex@unqork.com>
2022-01-28 13:55:59 -0500

    Add fallback to search for html element by name - Fallback to search for html element by name as specified in config.yaml - Update config.yaml labels to match current Grafana strings for each setting - Add __name__ == 'main' in case we want to import

--- main.py
+++ main.py
@@ -1,37 +1,11 @@
 import argparse
 import yaml
 import asyncio
+import re
 
 from bs4 import BeautifulSoup
 from lxml import etree
 from pyppeteer import launch
-
-parser = argparse.ArgumentParser(description='Process grafana settings')
-parser.add_argument('--config', '-c', help='Path to config file', required=False, default='config.yaml')
-parser.add_argument('--url', '-u', help='Grafana provider url', required=False)
-parser.add_argument('--provider-id', '-p', help='Grafana provider id', required=False)
-parser.add_argument('--commands', help='Prints commands that need to be run', required=False, default=False, action='store_true')
-parser.add_argument('--screenshot', help='Saves a screenshot of grafane for debug purposes', required=False, default=False, action='store_true')
-parser.add_argument(
-    '--update_settings',
-    help='Which settings should be automatically updated', required=False, default=[])
-
-args = parser.parse_args()
-if args.update_settings:
-    args.update_settings = args.update_settings.split(',')
-
-if not args.url and not args.provider_id:
-    print("Please provide a grafana url or a provider id")
-    exit(1)
-
-if not args.url:
-    args.url = 'https://grafana.scpri.me/d/Cg7V28sMk/provider-detail?var-provider=' + args.provider_id
-
-with open(args.config, "r") as stream:
-    try:
-        configs = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
 
 
 async def main():
@@ -73,10 +47,16 @@ async def main():
         dom_element = dom.xpath(element['xpath'])
 
         if not dom_element:
-            print('Element %s not found. Debug with --screenshot or check your configuration file!' % element_name)
-            continue
+            # fallback to search by name for the element
+            dom_element = soup.find(text=re.compile(re.escape(element['label']))).parent.next_sibling.div
+            if not dom_element:
+                print('Element %s not found. Debug with --screenshot or check your configuration file!' % element_name)
+                continue
+            else:
+                element_value = dom_element.contents[0].text
+        else:
+            element_value = dom_element[0].text
 
-        element_value = dom.xpath(element['xpath'])[0].text
         if args.commands and 'command' in element:
             print('spc host config %s %s%s' % (element['command'], element_value, element['unit']))
         elif not args.commands:
@@ -84,4 +64,32 @@ async def main():
 
     await browser.close()
 
-asyncio.get_event_loop().run_until_complete(main())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process grafana settings')
+    parser.add_argument('--config', '-c', help='Path to config file', required=False, default='config.yaml')
+    parser.add_argument('--url', '-u', help='Grafana provider url', required=False)
+    parser.add_argument('--provider-id', '-p', help='Grafana provider id', required=False)
+    parser.add_argument('--commands', help='Prints commands that need to be run', required=False, default=False, action='store_true')
+    parser.add_argument('--screenshot', help='Saves a screenshot of grafane for debug purposes', required=False, default=False, action='store_true')
+    parser.add_argument(
+        '--update_settings',
+        help='Which settings should be automatically updated', required=False, default=[])
+
+    args = parser.parse_args()
+    if args.update_settings:
+        args.update_settings = args.update_settings.split(',')
+
+    if not args.url and not args.provider_id:
+        print("Please provide a grafana url or a provider id")
+        exit(1)
+
+    if not args.url:
+        args.url = 'https://grafana.scpri.me/d/Cg7V28sMk/provider-detail?var-provider=' + args.provider_id
+
+    with open(args.config, "r") as stream:
+        try:
+            configs = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    asyncio.get_event_loop().run_until_complete(main())
